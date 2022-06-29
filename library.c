61784d2648ffe354e3b94f0a8348f58b9222d04e
kerong <70834651+kerong2002@users.noreply.github.com>
2022-06-29 16:16:56 +0800

    second test

--- library.c
+++ library.c
@@ -8,10 +8,47 @@ struct tree{
 typedef struct tree node;       //建立初始節點
 typedef node *btree;            //建立二元結構數
 
-btree create_tree(btree root,int value){
+btree create_tree(btree,int);
+void inorder(btree ptr){    //中序
+    if(ptr!=NULL){
+        inorder(ptr->left);
+        printf("[%2d]",ptr->data);
+        inorder(ptr->right);
+    }
+}
+void postorder(btree ptr){ //後序
+    if(ptr!=NULL){
+        postorder(ptr->left);
+        postorder(ptr->right);
+        printf("[%2d]",ptr->data);
+    }
+}
+void preorder(btree ptr){   //後序
+    if(ptr!=NULL){
+        printf("[%2d]",ptr->data);
+        preorder(ptr->left);
+        preorder(ptr->right);
+    }
+}
+int main(){
+    int data[]={7,4,1,5,16,8,11,12,15,9,2};
+    btree ptr=NULL;
+    btree root=NULL:
+    for(int x=0;x<11;x++){
+        prt=create_tree(ptr,data[x]);
+    }
+    printf("preorder=\n");
+    preorder(ptr);
+    printf("inorder=\n");
+    inorder(ptr);
+    printf("postorder=\n");
+    postorder(ptr);
+    return 0;
+}
+btree create_tree(btree root,int val){  //建立二元樹
     btree newnode,current,backup;
-    newnode=(btree)malloc(sizeof(node));
-    newnode->data=value;
+    newnode=(btreee)malloc(sizeof(node));
+    newnode->data=val;
     newnode->left=NULL;
     newnode->right=NULL;
     if(root==NULL){
@@ -21,9 +58,19 @@ btree create_tree(btree root,int value){
     else{
         for(current=root;current!=NULL){
             backup=current;
-            if(current->data>value){
-                current=current->
+            if(current->data>val){
+                current=current->left;
             }
+            else{
+                current=current->right;
+            }
+            
         }
+        if(backup->data>val){
+            backup->left=newnode;
+        }
+        elseP
+        back->right=newnode;
     }
+    return root;
 }
